087bccd16aebfcd71396fe1f5636dbf3de193a1a
Sebastian Van Rooyen <svanrooyen@ulwembubs.com>
2023-01-13 16:21:57 +0200

    Add to cart functionality added

---
+++ SpeedyWheelz/Controllers/CartController.cs
@@ -0,0 +1,79 @@
+﻿using SpeedyWheelz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpeedyWheelz.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CartController() : this(new ApplicationDbContext())
+        { 
+        }
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            Cart cart = GetCart();
+            return View(cart);
+        }
+
+        public ActionResult CartItems()
+        {
+            Cart cart = GetCart();
+
+            return PartialView("_CartItems", cart);
+        }
+
+        public ActionResult AddToCart(int productId, int quantity)
+        {
+            Product product = GetProduct(productId);
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.AddItem(product, quantity);
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+
+        public ActionResult RemoveFromCart(int productId)
+        {
+            Product product = GetProduct(productId);
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveItem(product);
+                return RedirectToAction("Index");
+            }
+            return HttpNotFound();
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+        private Product GetProduct(int productId)
+        {
+            // Get product from database or other source
+            var product = _db.Products.Where(x => x.ProductId == productId).FirstOrDefault();
+
+            return product;
+        }
+    }
+}
---
+++ SpeedyWheelz/Models/CartModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SpeedyWheelz.Models;
+
+namespace SpeedyWheelz.Models
+{
+    public class Cart
+    {
+        public List<CartItem> Items { get; set; }
+
+        public Cart()
+        {
+            Items = new List<CartItem>();
+        }
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartItem item = Items.Where(x => x.Product.ProductId == product.ProductId).FirstOrDefault();
+            if (item == null)
+            {
+                item = new CartItem()
+                {
+                    Product = product,
+                    Quantity = quantity
+                };
+                Items.Add(item);
+            }
+            else
+            {
+                item.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(Product product)
+        {
+            CartItem item = Items.Where(x => x.Product.ProductId == product.ProductId).FirstOrDefault();
+            if (item != null)
+            {
+                Items.Remove(item);
+            }
+        }
+
+        public decimal TotalPrice
+        {
+            get { return Items.Sum(x => x.TotalPrice); }
+        }
+    }
+
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public decimal TotalPrice
+        {
+            get { return Product.Price * Quantity; }
+        }
+    }
+
+}
