7cbf40a62f42c0254b323938b2f60c85d778cd80
Sebastian Van Rooyen <svanrooyen@ulwembubs.com>
2023-03-23 15:19:08 +0200

    Email on order functionality

--- SpeedyWheelz/Controllers/OrderController.cs
+++ SpeedyWheelz/Controllers/OrderController.cs
@@ -4,12 +4,16 @@ using SpeedyWheelz.Migrations;
 using SpeedyWheelz.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using System.Xml.Linq;
 
 namespace SpeedyWheelz.Controllers
@@ -136,12 +140,109 @@ namespace SpeedyWheelz.Controllers
             order.OrderStatus = "Paid";
             order.UserId = userId;
 
+            var controllerContext = ControllerContext;
+            var userEmail = User.Identity.Name;
+
+            // Render the view to a string using the ViewToString method
+            string renderedView = ViewToString(controllerContext, "CustomerMailView", cart);
+
+
+            SendMailCustomerOrder(userEmail, "Your Order Has Been Confirmed", renderedView);
+
             Session.Remove("Cart");
+
             _db.Orders.Add(order);
             _db.SaveChanges();
 
+
+
             return RedirectToAction("Index", "Home");
         }
+
+        public ActionResult CustomerMailView()
+        {
+            var loggedUser = User.Identity.GetUserId();
+
+            Cart cart = GetCart();
+
+            ViewBag.Address = _db.Addresses.Where(u => u.ApplicationUserId == loggedUser).Select(s => s.Street).FirstOrDefault();
+
+            return View(cart);
+        }
+
+        public static string ViewToString(ControllerContext context, string viewName, object model)
+        {
+            // Find the view engine for the view name
+            var viewEngineResult = ViewEngines.Engines.FindPartialView(context, viewName);
+
+            // Throw an exception if the view name is not found
+            if (viewEngineResult.View == null)
+            {
+                throw new InvalidOperationException("Could not find view " + viewName);
+            }
+
+            // Create a string writer to write the rendered view to
+            var stringBuilder = new StringBuilder();
+            var stringWriter = new StringWriter(stringBuilder);
+
+            // Create a new view result using the view name and model
+            var viewResult = new ViewResult
+            {
+                ViewName = viewName,
+                ViewData = new ViewDataDictionary(model),
+                TempData = context.Controller.TempData
+            };
+
+            // Create a new view context to render the view
+            var viewContext = new ViewContext(
+                context,
+                viewEngineResult.View,
+                viewResult.ViewData,
+                viewResult.TempData,
+                stringWriter
+            );
+
+            // Render the view to the string writer
+            viewEngineResult.View.Render(viewContext, stringWriter);
+
+            // Return the rendered view as a string
+            return stringBuilder.ToString();
+        }
+
+        public ActionResult PartialMailAddress()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var address = _db.Addresses.Where(u => u.ApplicationUserId == userId).FirstOrDefault();
+
+            return PartialView("PartialMailAddress", address);
+        }
+
+        public bool SendMailCustomerOrder(string toEmail, string subject, string emailBody)
+        {
+            try
+            {
+                var senderEmail = ConfigurationManager.AppSettings["Email"].ToString();
+
+                MailMessage msg = new MailMessage(senderEmail, toEmail, subject, emailBody);
+                msg.BodyEncoding = Encoding.UTF8;
+                msg.IsBodyHtml = true; // Set IsBodyHtml property to true to send email in HTML format
+
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", Convert.ToInt32(587));
+                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["Email"].ToString(), ConfigurationManager.AppSettings["EmailPassword"].ToString());
+                smtpClient.Credentials = credentials;
+                smtpClient.EnableSsl = true;
+                smtpClient.Send(msg);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                var error = e.Message;
+                return false;
+            }
+        }
+
 
         public ActionResult OrderSummary()
         {
