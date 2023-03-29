ee31172ee43aaeb210c2f64bf7199651276fe46f
Sebastian Van Rooyen <svanrooyen@ulwembubs.com>
2023-03-29 23:43:05 +0200

    Integrated loader on redirect click

--- SpeedyWheelz/App_Start/BundleConfig.cs
+++ SpeedyWheelz/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace SpeedyWheelz
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
 
             bundles.Add(new StyleBundle("~/").Include(
                       "~/css/animate.css",
